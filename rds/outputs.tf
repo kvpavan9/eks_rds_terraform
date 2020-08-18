e20d6adb84b3ae4c800234b9bc634376b04620c3
BALABHASKARARAO <balabhaskararaoguntupalli@gmail.com>
2020-08-18 16:01:33 +0530

    rds output secton db added

--- rds/outputs.tf
+++ rds/outputs.tf
@@ -18,18 +18,18 @@ output "db_instance_address" {
 }
 */
 locals {
-  db_instance_address           = "${element(concat(coalescelist(aws_db_instance.db.*.address, aws_db_instance.this.*.address), list("")), 0)}"
-  db_instance_arn               = "${element(concat(coalescelist(aws_db_instance.db.*.arn, aws_db_instance.this.*.arn), list("")), 0)}"
-  db_instance_availability_zone = "${element(concat(coalescelist(aws_db_instance.db.*.availability_zone, aws_db_instance.this.*.availability_zone), list("")), 0)}"
-  db_instance_endpoint          = "${element(concat(coalescelist(aws_db_instance.db.*.endpoint, aws_db_instance.this.*.endpoint), list("")), 0)}"
-  db_instance_hosted_zone_id    = "${element(concat(coalescelist(aws_db_instance.db.*.hosted_zone_id, aws_db_instance.this.*.hosted_zone_id), list("")), 0)}"
-  db_instance_id                = "${element(concat(coalescelist(aws_db_instance.db.*.id, aws_db_instance.this.*.id), list("")), 0)}"
-  db_instance_resource_id       = "${element(concat(coalescelist(aws_db_instance.db.*.resource_id, aws_db_instance.this.*.resource_id), list("")), 0)}"
-  db_instance_status            = "${element(concat(coalescelist(aws_db_instance.db.*.status, aws_db_instance.this.*.status), list("")), 0)}"
-  db_instance_name              = "${element(concat(coalescelist(aws_db_instance.db.*.name, aws_db_instance.this.*.name), list("")), 0)}"
-  db_instance_username          = "${element(concat(coalescelist(aws_db_instance.db.*.username, aws_db_instance.this.*.username), list("")), 0)}"
-  db_instance_password          = "${element(concat(coalescelist(aws_db_instance.db.*.password, aws_db_instance.this.*.password), list("")), 0)}"
-  db_instance_port              = "${element(concat(coalescelist(aws_db_instance.db.*.port, aws_db_instance.this.*.port), list("")), 0)}"
+  db_instance_address           = "${element(concat(coalescelist(aws_db_instance.db.*.address, aws_db_instance.db.*.address), list("")), 0)}"
+  db_instance_arn               = "${element(concat(coalescelist(aws_db_instance.db.*.arn, aws_db_instance.db.*.arn), list("")), 0)}"
+  db_instance_availability_zone = "${element(concat(coalescelist(aws_db_instance.db.*.availability_zone, aws_db_instance.db.*.availability_zone), list("")), 0)}"
+  db_instance_endpoint          = "${element(concat(coalescelist(aws_db_instance.db.*.endpoint, aws_db_instance.db.*.endpoint), list("")), 0)}"
+  db_instance_hosted_zone_id    = "${element(concat(coalescelist(aws_db_instance.db.*.hosted_zone_id, aws_db_instance.db.*.hosted_zone_id), list("")), 0)}"
+  db_instance_id                = "${element(concat(coalescelist(aws_db_instance.db.*.id, aws_db_instance.db.*.id), list("")), 0)}"
+  db_instance_resource_id       = "${element(concat(coalescelist(aws_db_instance.db.*.resource_id, aws_db_instance.db.*.resource_id), list("")), 0)}"
+  db_instance_status            = "${element(concat(coalescelist(aws_db_instance.db.*.status, aws_db_instance.db.*.status), list("")), 0)}"
+  db_instance_name              = "${element(concat(coalescelist(aws_db_instance.db.*.name, aws_db_instance.db.*.name), list("")), 0)}"
+  db_instance_username          = "${element(concat(coalescelist(aws_db_instance.db.*.username, aws_db_instance.db.*.username), list("")), 0)}"
+  db_instance_password          = "${element(concat(coalescelist(aws_db_instance.db.*.password, aws_db_instance.db.*.password), list("")), 0)}"
+  db_instance_port              = "${element(concat(coalescelist(aws_db_instance.db.*.port, aws_db_instance.db.*.port), list("")), 0)}"
 }
 
 output "db_instance_address" {
